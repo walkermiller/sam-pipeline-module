a16d816d8b566ea7892f94ae5f3c054054a15d00
Walker Miller <walkmlr@38f9d369330f.ant.amazon.com>
2021-05-28 13:12:04 -0400

    first

---
+++ sam_pipeline_module/sam_pipeline_module.py
@@ -0,0 +1,55 @@
+from aws_cdk import (core as cdk, 
+                    aws_codepipeline as codepipeline,
+                    aws_codepipeline_actions as codepipeline_actions,
+                    aws_codebuild as codebuild, 
+                    aws_codecommit as codecommit)
+
+
+class SamPipelineModule(cdk.Stack):
+    def addDeployStage(pipeline: codepipeline.Pipeline, account, region):
+        pass
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        app = cdk.CfnParameter(self, "app", type="String").value_as_string
+
+        repo = codecommit.Repository(self, id="cc", repository_name=app)
+
+        sourceOutput = codepipeline.Artifact()
+        pipeline = codepipeline.Pipeline(self, id="p", pipeline_name="{}-pipeline".format(app))
+        sourceAction = codepipeline_actions.CodeCommitSourceAction(
+            action_name='CodeCommit',
+            repository=repo,
+            output=sourceOutput
+        )
+        pipeline.add_stage(stage_name="Source", actions=[sourceAction])
+
+        pipeline.artifact_bucket.s3_url_for_object()
+
+        buildOutput = codepipeline.Artifact()
+
+        ## Create the CodeBuild Porject
+        buildSpec = codebuild.BuildSpec.from_object({
+            "version": "0.2",
+            "phases": {
+                "install": {
+                    "runtime-versions": {
+                        "python": 3.8
+                    }
+                },
+                "build": {
+                    "commands": ["sam package --template-file template.yml --s3-bucket s3://{} --output-template-file {}.yml".format(pipeline.artifact_bucket.bucket_name, app)]
+                }
+            }
+        })
+        project = codebuild.PipelineProject(self, id="bp", build_spec=buildSpec)
+
+        ## Add CodeBuild Project as a Pipeline Stage
+        buildAction = codepipeline_actions.CodeBuildAction(
+            action_name="CodeBuild",
+            project=project,
+            input=sourceOutput,
+            outputs=[buildOutput]
+        )
+        pipeline.add_stage(stage_name="Build", actions=[buildAction])
